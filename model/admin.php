ce9d004ca58ef8a23d86f14a770286115b5fc07c
Joelson Sartori Junior <joelsonsartori@gmail.com>
2020-08-13 15:16:11 -0300

    Desisti e voltei para a versao anterior

--- model/admin.php
+++ model/admin.php
@@ -6,6 +6,34 @@ class Admin extends User{
         parent::__construct($phone, $address, $email, $password, $type);
     }
 
+    // Administrator
+    function create_admin(){}
+
+    function update_admin(){}
+
+    function remove_admin(){}
+
+    // Doctor
+    function create_doctor(){}
+
+    function update_doctor(){}
+
+    function remove_doctor(){}
+
+    //Laboratory
+    function create_lab(){}
+    
+    function update_lab(){}
+
+    function remove_lab(){}
+    
+    //Patient
+    function create_patient(){}
+
+    function update_patient(){}
+
+    function remove_patient(){}
+
 }
 
 ?>
