22081cbdc34e3b6ab3ceab548f7450944dc6ee1e
Joelson Sartori Junior <joelsonsartori@gmail.com>
2020-08-14 16:25:06 -0300

    Criaçao da class controller responsavel pelas views

---
+++ App/core/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Application\core;
+
+use Application\models\Users;
+
+class Controller
+{
+
+  
+  public function model($model){
+    require '../App/models/' . $model . '.php';
+    $classe = 'App\\models\\' . $model;
+
+    return new $classe();
+  }
+
+  public function view(string $view, $data = []){
+    require '../App/views/' . $view . '.php';
+  }
+
+  public function pageNotFound(){
+    $this->view('erro404');
+  }
+}
