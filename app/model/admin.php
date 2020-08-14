cfe1d051c1f59fbffbbc7e171ab8fe8026eea878
Joelson Sartori Junior <joelsonsartori@gmail.com>
2020-08-14 14:39:33 -0300

    Inicio criaçao model admin

---
+++ app/model/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+class Admin{
+    private $name;
+    private $address;
+    private $phone;
+    private $email;
+    private $specialty;
+    private $crm;
+    
+    public function save(){}
+
+    public function update(){}
+
+    public function remove(){}
+
+    public function listAll(){}
+}
--- public/index.php
+++ public/index.php
@@ -1,13 +1,2 @@
 <?php
 
-require_once('../vendor/autoload.php');
-require_once('../app/config/config.php');
-require_once('../app/functions/functions.php');
-
-
-use app\controller\Teste;
-
-(new \app\core\RouterCore());
-
-
-$controller = new Teste();
