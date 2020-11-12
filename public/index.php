88099eacc4743e2d3a0e39d2dbe551018cdf33ae
Joelson Sartori <joelsonsartori@gmail.com>
2020-11-12 20:16:15 -0300

    feat: inicio criação index

--- public/index.php
+++ public/index.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
+
+<head>
+
     <meta charset="utf-8">
-    <title>Simple Framework</title>
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-  </head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sistema de Saúde - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/global.css" rel="stylesheet">
+
+</head>
   <body>
 
   <?php
@@ -18,5 +34,14 @@
   ?>
   <script src="/assets/js/jquery.slim.min.js"></script>
   <script src="/assets/js/bootstrap.min.js"></script>
+      <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
   </body>
 </html>
