a143e639297155287f98e7c94d274d02a75baa83
Joelson Sartori <joelsonsartori@gmail.com>
2020-11-18 13:59:35 -0300

    feat: layout sidebar

--- Application/views/admin/index.php
+++ Application/views/admin/index.php
@@ -1,27 +1,30 @@
 <div class="d-flex">
   <div id="sidebar-container" class="bg-gradient">
-    <div class="logo">
+    <div class="logo d-flex justify-content-center">
       <img src="/assets/img/medica.jpg">
-      <p>Joana administradora</p>
+      <div class="nome" >
+        <p class="font-weight-bold"> Admin</p>
+        <p>Joana administradora</p>
+      </div>
     </div>
-    <div class="menu">
-      <div class="d-flex">
+    <div class="menu ">
+      <div class="d-flex justify-content-center">
         <ion-icon name="key-outline"></ion-icon>
         <a href="#" class="d-block text-light p-3">Cadastro Admin</a>
       </div>
-      <div class="d-flex">
+      <div class="d-flex pl-4 justify-content-center">
         <ion-icon name="flask-outline"></ion-icon>
         <a href="#" class="d-block text-light p-3">Cadastro Laborátorio</a>
       </div>
-      <div class="d-flex">
+      <div class="d-flex justify-content-center">
         <ion-icon ion-icon name="fitness-outline"></ion-icon>
         <a href="#" class="d-block text-light p-3">Cadastro Médico</a>
       </div>
-      <div class="d-flex">
+      <div class="d-flex justify-content-center">
         <ion-icon name="person-outline"></ion-icon>
         <a href="#" class="d-block text-light p-3">Cadastro Paciente</a>
       </div>
-      <div class="d-flex">
+      <div class="d-flex justify-content-center">
         <ion-icon name="reader-outline"></ion-icon>
         <a href="#" class="d-block text-light p-3">Cadastro Consulta</a>
       </div>
