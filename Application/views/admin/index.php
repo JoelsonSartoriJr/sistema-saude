97741d67e1ecb9cad6fad747db1965ca62b4e206
Joelson Sartori <joelsonsartori@gmail.com>
2020-11-18 15:09:59 -0300

    feat: topbar

--- Application/views/admin/index.php
+++ Application/views/admin/index.php
@@ -1,8 +1,10 @@
 <div class="d-flex">
+
+  <!--Sidebar-->
   <div id="sidebar-container" class="bg-gradient">
     <div class="logo d-flex justify-content-center">
       <img src="/assets/img/medica.jpg">
-      <div class="nome" >
+      <div class="nome">
         <p class="font-weight-bold"> Admin</p>
         <p>Joana administradora</p>
       </div>
@@ -30,4 +32,10 @@
       </div>
     </div>
   </div>
+
+  <!--TopBar-->
+  <div id="topbar-container" class="shadow p-3 mb-5 bg-white rounded">
+    <p class="h1">Cadastro de Médico</p>
+  </div>
+
 </div>
