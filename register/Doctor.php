e09f5486de6d9fef8bf1e4c153608d17486ef0b4
Joelson Sartori <joelsonsartori@gmail.com>
2021-02-21 16:22:56 -0300

    feat: ajuste cadastro de médico com mysql

--- register/Doctor.php
+++ register/Doctor.php
@@ -2,53 +2,42 @@
 
 session_start();
 
-
-require_once '../utils/Utils.php';
-require_once '../model/Doctor.php';
+require_once('../login/conexao.php');
+require_once('../utils/Utils.php');
 
 try {
+    $crm = $_POST['crm'];
 
-    $method = $_SERVER['REQUEST_METHOD'];
-    
-    if (file_exists('../date/date.xml')){
-        $xml = simplexml_load_file('../date/date.xml')  or die ("Failed to load");
-        if($method == 'POST'){
-            $crm = $_POST['crm'];
-            $node = $xml->xpath("//user[crm = '$crm']");
+    $query = $conn->prepare("SELECT * FROM doctor WHERE crm = ? ");
+    $query->execute(array($crm));
 
-            if(count($node) > 0){
-                $_SESSION['erro'] = "Usuario já cadastrado!";
-            }else{
-                $user = $xml->users->addChild('user');
-                $id = md5(uniqid(""));
-                $name = $_POST['name'];
-                $type = 'doctor';
-                $phone = $_POST['phone'];
-                $email = $_POST['email'];
-                $address = $_POST['address'];
-                $password = $_POST['password'];
-                $specialty = $_POST['specialty'];
-
-                $lab = new Doctor($id, $type, $name, $phone, $email, $address, $password, $specialty, $crm);
-
-                foreach ($lab as $key => $value){
-                    $user->addChild($key, $value);
-                }
-
-                $xml->asXML('../date/date.xml');
-                $_SESSION['erro'] = "Usuario cadastrado com sucesso!";
-            }
-            
-        }
+    if ($query->rowCount()) {
+        $_SESSION['erro'] = 'Médico já cadastrado';
+        header("Location: http://localhost:8000/views/admin/admin.php");
     } else {
-        $_SESSION['erro'] = "Erro ao conectar ao banco!";
-    }
-    header("Location: http://localhost:8000/views/admin/admin.php");
+        $type = 'doctor';
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $address = $_POST['address'];
+        $password = $_POST['password'];
+        $specialty = $_POST['specialty'];
 
+        $queryUser = $conn->prepare("INSERT INTO users (name, email, password, type) VALUES(?, ?, ?, ?)");
+        $queryUser->execute(array($name, $email, $password, $type));
+
+        $id = $conn->lastInsertId();
+
+        $queryDoc = $conn->prepare("INSERT INTO doctor (id, crm, phone, address, specialty) VALUES(?, ?, ?, ?, ?)");
+        $queryDoc->execute(array($id, $crm, $phone, $address, $specialty));
+
+        $_SESSION['erro'] = "Médico cadastrado com sucesso!";
+        header("Location: http://localhost:8000/views/admin/admin.php");
+    }
 } catch (Throwable $e) {
-    console_log('Throwable'.$e);
-    header("Location: http://localhost:8000");
+    console_log('Throwable' . $e);
+    header("Location: http://localhost:8080");
 } catch (Exception $e) {
-    console_log('Exception'.$e);
-    header("Location: http://localhost:8000");
+    console_log('Exception' . $e);
+    header("Location: http://localhost:8080");
 }
