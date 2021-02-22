c033a3424d2a330f8abf724493a00f84d379070e
Joelson Sartori <joelsonsartori@gmail.com>
2021-02-22 19:33:47 -0300

    feat: modificação para sql

--- register/Patient.php
+++ register/Patient.php
@@ -9,7 +9,7 @@ try {
     $cpf = $_POST['cpf'];
     $email = $_POST['email'];
 
-    $queryPatien = $conn->prepare("SELECT * FROM patient WHERE cpf = ? ");
+    $queryPatient = $conn->prepare("SELECT * FROM patient WHERE cpf = ? ");
     $queryPatient->execute(array($cpf));
 
     $queryUser = $conn->prepare("SELECT * FROM users WHERE email = ?");
@@ -31,72 +31,17 @@ try {
         $queryUser = $conn->prepare("INSERT INTO users (name, email, password, type) VALUES(?, ?, ?, ?)");
         $queryUser->execute(array($name, $email, $password, $type));
 
-        $id = $conn->lastInsertId();
+        $id_user = $conn->lastInsertId();
 
-        $queryDoc = $conn->prepare("INSERT INTO doctor (id, crm, phone, address, specialty) VALUES(?, ?, ?, ?, ?)");
-        $queryDoc->execute(array($id, $crm, $phone, $address, $specialty));
-
-        $_SESSION['erro'] = "Médico cadastrado com sucesso!";
+        $queryPatient = $conn->prepare("INSERT INTO patient (cpf, id_user, phone, address, gender, date_nasc) VALUES(?, ?, ?, ?, ?, ?)");
+        $queryPatient->execute(array($cpf, $id_user, $phone, $address, $gender, $date_nasc));
+        $_SESSION['erro'] = "Paciente cadastrado com sucesso!";
         header("Location: http://localhost:8000/views/admin/admin.php");
     }
 } catch (Throwable $e) {
     console_log('Throwable' . $e);
-    header("Location: http://localhost:8080");
-} catch (Exception $e) {
-    console_log('Exception' . $e);
-    header("Location: http://localhost:8080");
-}
-
-session_start();
-
-
-require_once '../utils/Utils.php';
-require_once '../model/Patient.php';
-
-try {
-
-    $method = $_SERVER['REQUEST_METHOD'];
-    
-    if (file_exists('../date/date.xml')){
-        $xml = simplexml_load_file('../date/date.xml')  or die ("Failed to load");
-        if($method == 'POST'){
-            $cpf = $_POST['cpf'];
-
-            $node = $xml->xpath("//user[cpf = '$cpf']");
-
-            if(count($node) > 0){
-                $_SESSION['erro'] = "Paciente já cadastrado!";
-            }else{
-                $user = $xml->users->addChild('user');
-
-                $id = md5(uniqid(""));
-                $type = 'patient';
-                $name = $_POST['name'];
-                $phone = $_POST['phone'];
-                $address = $_POST['address'];
-                $password = $_POST['password'];
-                $gender = $_POST['gender'];
-                $date_nasc = $_POST['date_nasc'];
-                $email = $_POST['email'];
-
-                $patient = new Patient($id, $type, $name, $phone, $email,$address, $password, $gender, $date_nasc, $cpf);
-
-                foreach ($patient as $key => $value){
-                    $user->addChild($key, $value);
-                }
-                $xml->asXML('../date/date.xml');
-                $_SESSION['erro'] = "Paciente cadastrado com sucesso!";
-            }
-        }
-    } else {
-        $_SESSION['erro'] = "Erro ao conectar ao banco!";
-    }
-    header("Location: http://localhost:8000/views/admin/admin.php");
-
-} catch (Throwable $e) {
-    console_log('Throwable'.$e);
     header("Location: http://localhost:8000");
 } catch (Exception $e) {
-    console_log('Exception'.$e);
+    console_log('Exception' . $e);
     header("Location: http://localhost:8000");
 }
